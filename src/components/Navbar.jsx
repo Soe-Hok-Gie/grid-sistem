898b750f49df937804db15b55b859c0b754d7050
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-02-25 17:58:58 +0700

    stelan project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div class="container">
+      <div class="row">
+        <div class="col">Column</div>
+        <div class="col">Column</div>
+        <div class="col">Column</div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
