206e931161365450c00917c41fe1bab947d30a9d
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-02-28 23:43:39 +0700

    menambahkan component profile

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Galeri from "./components/Galeri";
 import Navbar from "./components/Navbar";
 import "././components/Style.css";
+import Profil from "./components/Profil";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Galeri />
+      <Profil />
     </>
   );
 };
