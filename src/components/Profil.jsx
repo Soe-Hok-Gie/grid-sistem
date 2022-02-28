af8512bccf3345a3e0793926d908031f495be1bc
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-02-28 23:54:39 +0700

    menambahkan row dgn col-12 dan perubahan detail kecil

--- src/components/Profil.jsx
+++ src/components/Profil.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Profil = () => {
-  return <div>Profil</div>;
+  return (
+    <div className="container-fluid profil">
+      <div className="container">
+        <div className="row py-5">
+          <div className="col text-center">
+            <h1>Layout website</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, obcaecati.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profil;
