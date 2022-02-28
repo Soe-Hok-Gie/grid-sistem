e76e16c6e690e315352272920055fbb0b50335a1
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-02-28 23:20:46 +0700

    menambahkan row baru dgn 6 col-2 dan perubahan detail kecil

--- src/components/Galeri.jsx
+++ src/components/Galeri.jsx
@@ -2,12 +2,32 @@ import React from "react";
 
 const Galeri = () => {
   return (
-    <div className="container-fluid-galeri py-5">
+    <div className="container-fluid galeri py-5">
       <div className="container">
         <div className="row">
-          <div className="col">
+          <div className="col text-center">
             <h1>Galeri</h1>
             <p>koleksi foto</p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-2">
+            <img src="./assets/1.png" alt="" class="img-fluid" />
+          </div>
+          <div className="col-2">
+            <img src="./assets/1.png" alt="" class="img-fluid" />
+          </div>
+          <div className="col-2">
+            <img src="./assets/1.png" alt="" class="img-fluid" />
+          </div>
+          <div className="col-2">
+            <img src="./assets/1.png" alt="" class="img-fluid" />
+          </div>
+          <div className="col-2">
+            <img src="./assets/1.png" alt="" class="img-fluid" />
+          </div>
+          <div className="col-2">
+            <img src="./assets/1.png" alt="" class="img-fluid" />
           </div>
         </div>
       </div>
