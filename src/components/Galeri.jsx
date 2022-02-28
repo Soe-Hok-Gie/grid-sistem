ce581a0e3aa1e5611863049ba726202f4a7f3dbc
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-02-28 22:45:34 +0700

    membuat components Galeri sesuai kebutuhan

--- src/components/Galeri.jsx
+++ src/components/Galeri.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Galeri = () => {
-  return <div>Galeri</div>;
+  return (
+    <div className="container-fluid">
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h1>Galeri</h1>
+            <p>koleksi foto</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Galeri;
